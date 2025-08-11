bc641e445591f85275442d7e7c8611f9e0965a01
Matthew Aquiatan <matthewaquiatan@gmail.com>
2025-08-12 03:18:53 +0800

    Add blank UE C++ project

---
+++ Source/GeoTechExam/GeoTechExam.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GeoTechExam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GeoTechExam, "GeoTechExam" );
